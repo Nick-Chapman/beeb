f896358c522694fcc3381e5360ef3ff0c07a6b83
Nick Chapman <nic@amadido.co.uk>
2023-01-29 14:58:48 +0000

    game1: keys(U,D,L,R) controlling coarse X/Y

--- src/game1.asm
+++ src/game1.asm
@@ -3,23 +3,35 @@ osasci = &ffe3
 oswrch = &ffee
 osbyte = &fff4
 
-keyCodeX = -67
-keyCodeZ = -98
+keyCodeU = -58
+keyCodeD = -42
+keyCodeL = -26
+keyCodeR = -122
 
 screenStart = &3000
 screenEnd = &8000
 
 ORG &70
 
-.curr SKIP 2 ; current screen address (which we fill)
-.last SKIP 2 ; last screen address (which we erase)
-.write SKIP 2
+.keyU SKIP 1
+.keyD SKIP 1
+.keyL SKIP 1
+.keyR SKIP 1
 
-.pressedX SKIP 1
-.pressedZ SKIP 1
+.lastKeyU SKIP 1
+.lastKeyD SKIP 1
+.lastKeyL SKIP 1
+.lastKeyR SKIP 1
+
+.currCX SKIP 1
+.currCY SKIP 1 ; coarse-Y
+.currA SKIP 2 ; current screen address (which we fill)
+.lastA SKIP 2 ; last screen address (which we erase)
+
+.write SKIP 2 ; pointer for writing to screen
 
 ORG &1900
-GUARD &1B00
+GUARD &1C00
 GUARD screenStart
 
 .start:
@@ -31,71 +43,141 @@ GUARD screenStart
     jsr drawGrid
     jsr drawCurr
 .loop:
+    jsr saveLastKeys
     jsr readKeys
-    jsr updateGameState
+    jsr updateGameStateWithRepeat
     jsr prepareForDraw
     jsr syncDelay
     jsr drawScreen
     jmp loop
     }
 
-.readKeys:
-    jsr checkX
-    jsr checkZ
+.saveLastKeys:
+    lda keyU : sta lastKeyU
+    lda keyD : sta lastKeyD
+    lda keyL : sta lastKeyL
+    lda keyR : sta lastKeyR
     rts
 
-.checkX: {
-    lda #0 : sta pressedX
+.readKeys:
+    jsr checkU
+    jsr checkD
+    jsr checkL
+    jsr checkR
+    rts
+
+.checkU: {
+    lda #0 : sta keyU
     lda #&81
-    ldx #(keyCodeX AND &ff)
-    ldy #(keyCodeX AND &ff00) div 256
+    ldx #(keyCodeU AND &ff)
+    ldy #(keyCodeU AND &ff00) DIV 256
     jsr osbyte
     cpx #&ff : bne no
-    lda #1 : sta pressedX : .no:
+    lda #1 : sta keyU : .no:
     rts }
 
-.checkZ: {
-    lda #0 : sta pressedZ
+.checkD: {
+    lda #0 : sta keyD
     lda #&81
-    ldx #(keyCodeZ AND &ff)
-    ldy #(keyCodeZ AND &ff00) div 256
+    ldx #(keyCodeD AND &ff)
+    ldy #(keyCodeD AND &ff00) DIV 256
     jsr osbyte
     cpx #&ff : bne no
-    lda #1 : sta pressedZ : .no:
+    lda #1 : sta keyD : .no:
     rts }
+
+.checkL: {
+    lda #0 : sta keyL
+    lda #&81
+    ldx #(keyCodeL AND &ff)
+    ldy #(keyCodeL AND &ff00) DIV 256
+    jsr osbyte
+    cpx #&ff : bne no
+    lda #1 : sta keyL : .no:
+    rts }
+
+.checkR: {
+    lda #0 : sta keyR
+    lda #&81
+    ldx #(keyCodeR AND &ff)
+    ldy #(keyCodeR AND &ff00) DIV 256
+    jsr osbyte
+    cpx #&ff : bne no
+    lda #1 : sta keyR : .no:
+    rts }
+
+
+.updateGameState:
+    jsr saveLastScreenAddr
+    { lda keyU : beq no : lda lastKeyU : bne no : jsr onU : .no }
+    { lda keyD : beq no : lda lastKeyD : bne no : jsr onD : .no }
+    { lda keyL : beq no : lda lastKeyL : bne no : jsr onL : .no }
+    { lda keyR : beq no : lda lastKeyR : bne no : jsr onR : .no }
+    rts
+
+.updateGameStateWithRepeat:
+    jsr saveLastScreenAddr
+    { lda keyU : beq no : jsr onU : .no }
+    { lda keyD : beq no : jsr onD : .no }
+    { lda keyL : beq no : jsr onL : .no }
+    { lda keyR : beq no : jsr onR : .no }
+    rts
 
 .initVars:
-    lda #HI(screenStart) : sta curr+1
-    lda #LO(screenStart) : sta curr
+    lda #0 : sta currCX : sta currCY
+    lda #HI(screenStart) : sta currA+1
+    lda #LO(screenStart) : sta currA
     rts
 
-.updateGameState: {
-    jsr saveLast
-    lda pressedX : beq noX : jsr incrementCurr : .noX
-    lda pressedZ : beq noZ : jsr decrementCurr : .noZ
-    rts }
-
-.saveLast:
-    lda curr : sta last
-    lda curr+1 : sta last+1
+.saveLastScreenAddr:
+    lda currA : sta lastA
+    lda currA+1 : sta lastA+1
     rts
 
-.incrementCurr: {
-    inc curr : bne done
-    inc curr+1 : lda curr+1 : cmp #HI(screenEnd) : bne done
-    lda #HI(screenStart) : sta curr+1
-.done
+.onU:
+    lda currCY : bne nowrap
+    lda currA+1 : clc : adc #HI(screenEnd-screenStart) : sta currA+1 ; wrap
+.nowrap:
+    jsr upLine
+    lda currCY : sec : sbc #1 : and #31 : sta currCY
+    rts
+
+.onD: {
+    lda currCY : clc : adc #1 : and #31 : sta currCY : bne nowrap
+    lda currA+1 : sec : sbc #HI(screenEnd-screenStart) : sta currA+1 ; wrap
+.nowrap:
+    jsr downLine
     rts }
 
-.decrementCurr: {
-    lda curr : bne low
-    lda curr+1 : cmp #HI(screenStart) : bne high
-    lda #HI(screenEnd) : sta curr+1
-.high:
-    dec curr+1
-.low:
-    dec curr
+.onL: {
+    lda currCX : bne nowrap
+    lda #80 : sta currCX
+    jsr downLine
+.nowrap:
+    dec currCX
+    lda currA : sec : sbc #8 : sta currA
+    lda currA+1     : sbc #0 : sta currA+1
     rts }
+
+.onR: {
+    inc currCX : lda currCX : cmp #80 : bne nowrap
+    lda #0 : sta currCX
+    jsr upLine
+.nowrap:
+    lda currA : clc : adc #8 : sta currA
+    lda currA+1     : adc #0 : sta currA+1
+    rts }
+
+.upLine:
+    lda currA : sec : sbc #&80 : sta currA
+    lda currA+1     : sbc #2   : sta currA+1
+    rts
+
+.downLine:
+    lda currA : clc : adc #&80 : sta currA
+    lda currA+1     : adc #2   : sta currA+1
+    rts
+
 
 .prepareForDraw:
     rts
@@ -106,21 +188,21 @@ GUARD screenStart
     rts
 
 .drawLast:
-    lda last : sta write
-    lda last+1 : sta write+1
+    lda lastA : sta write
+    lda lastA+1 : sta write+1
     jsr eorWrite
     rts
 
 .drawCurr:
-    lda curr : sta write
-    lda curr+1 : sta write+1
+    lda currA : sta write
+    lda currA+1 : sta write+1
     jsr eorWrite
     rts
 
 .eorWrite:
     ldy #0
     lda (write),y
-    eor #255
+    eor #&88 ; single dot
     sta (write),y
     rts
 
