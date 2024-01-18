9090e09690f9c61ada4b4fe50ae9ffeb57c41a54
Nick Chapman <nic@amadido.co.uk>
2024-01-18 13:19:12 +0000

    reinstate incremental screen-address calculation

--- src/render.asm
+++ src/render.asm
@@ -4,11 +4,12 @@
 SyncAssert = TRUE
 
 screenStart = &3000
+screenEnd = &8000
 
 ;;; MOS vectors & zero page use
 interruptSaveA = &fc
 irq1v = &204
-    
+
 ;;; Sheila
 ula                         = &fe21
 system_VIA_portB            = &fe40
@@ -32,7 +33,8 @@ red = 6
 black = 7
 
 macro Ula col : lda #col : sta ula : endmacro
-    
+;;macro Ula col : endmacro
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Copy
 
@@ -40,6 +42,12 @@ macro Copy16i I,V
     lda #LO(I) : sta V
     lda #HI(I) : sta V+1
 endmacro
+
+macro Copy16v A,B
+    lda A : sta B
+    lda A+1 : sta B+1
+endmacro
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; zero page
@@ -50,10 +58,19 @@ org &70
 .bufO skip 1
 .bufP skip 2
 
-.theX skip 2 ; plot form Hi byte + hi-bit of LO byte
+.theX skip 2 ; plot from Hi byte + hi-bit of LO byte
 .theY skip 2 ; plot only from HI byte
-.theA skip 2
-.theFineXmask skip 1
+
+.theCX SKIP 1 ; coarse-X : 0..79
+.theCY SKIP 1 ; coarse-Y : 0..31
+.theFX SKIP 1 ; fine-X   : 0..3
+.theFY SKIP 1 ; fine-Y   : 0..7
+.theA SKIP 2  ; screen address of the 8x8 char
+
+    
+.outlineIndex skip 1
+.outlineMotion skip 1
+    
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Start
@@ -66,6 +83,7 @@ org &1100
 
 .spin: jmp spin
 
+    
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; printString, printHexA, emit
 
@@ -114,9 +132,8 @@ macro Emit C
 endmacro
 
 macro Space
-    Emit '.'
+    Emit ' '
 endmacro
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Crash
@@ -127,6 +144,45 @@ macro Crash S
     jmp spin
 .msg:EQUS S, 0
 endmacro
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Keyboard input
+
+.keyUp    EQUB 0
+.keyDown  EQUB 0
+.keyLeft  EQUB 0
+.keyRight EQUB 0
+
+.keyUpLAST    EQUB 0
+.keyDownLAST  EQUB 0
+.keyLeftLAST  EQUB 0
+.keyRightLAST EQUB 0
+    
+macro PollKey CODE, VAR
+    lda #0 : sta VAR
+    lda #(-CODE-1) : sta system_VIA_portA : lda system_VIA_portA
+    bpl no : lda #1 : sta VAR : .no
+endmacro
+
+.readKeys:
+    ;lda keyLeft : sta keyLeftLAST
+    ;lda keyRight : sta keyRightLAST
+    ;lda keyUp : sta keyUpLAST
+    ;lda keyDown : sta keyDownLAST
+    PollKey -58,  keyUp
+    PollKey -42,  keyDown
+    PollKey -26,  keyLeft
+    PollKey -122, keyRight
+    rts
+
+.printKeyState:
+    ;Space
+    lda #'.' : { ldy keyLeft : beq no : lda #'L' : .no } : jsr emit
+    lda #'.' : { ldy keyRight: beq no : lda #'R' : .no } : jsr emit
+    lda #'.' : { ldy keyUp   : beq no : lda #'U' : .no } : jsr emit
+    lda #'.' : { ldy keyDown : beq no : lda #'D' : .no } : jsr emit
+    rts
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; IRQ, VBlank syncing
 
@@ -146,9 +202,9 @@ endmacro
     }
 
 .syncVB: {
-    IF SyncAssert : lda vsyncNotify : bne failSync1 : ENDIF ; pre-sync check (more harsh)
+    ;;IF SyncAssert : lda vsyncNotify : bne failSync1 : ENDIF ; pre-sync check (more harsh)
     { .loop : lda vsyncNotify : beq loop }
-    IF SyncAssert : cmp #2 : bcs failSync2 : ENDIF ; post-sync check (move forgiving)
+    ;;IF SyncAssert : cmp #2 : bcs failSync2 : ENDIF ; post-sync check (move forgiving)
     lda #0 : sta vsyncNotify
     rts
 .failSync1:
@@ -190,7 +246,11 @@ endmacro
 .init:
     lda #%01111111 : sta system_VIA_interruptEnable ; disable all interrupts
     lda #%10000010 : sta system_VIA_interruptEnable ; enable just VBlank
-    
+	
+    ;; start in keys mode
+    lda #%00000011 : sta system_VIA_portB ; set bit 3 to 0
+    lda #%01111111 : sta system_VIA_dataDirectionA ; (top bit input)
+
     jsr mode1
     jsr cursorOff
     jsr replaceWhiteWithCyan
@@ -202,7 +262,7 @@ endmacro
 
 ;;; TODO: different sizes for different buffers
 ;;; for now, all have same size
-bufferSize = 44*3+1
+bufferSize = 200
 
 .b0: skip bufferSize
 .b1: skip bufferSize
@@ -215,13 +275,13 @@ numberOfBuffers = (buffersEnd-buffers)/2
 PRINT numberOfBuffers
 .offsets: skip numberOfBuffers ; TODO: do we need to track all these?
 
-.debugBufferInfo:
-    Space : ldy #0 : lda offsets,y : jsr printHexA
-    Space : ldy #1 : lda offsets,y : jsr printHexA
-    Space : ldy #2 : lda offsets,y : jsr printHexA
-    Space : ldy #3 : lda offsets,y : jsr printHexA
-    Space : ldy #4 : lda offsets,y : jsr printHexA
-    rts
+;; .debugBufferInfo:
+;;     Space : ldy #0 : lda offsets,y : jsr printHexA
+;;     Space : ldy #1 : lda offsets,y : jsr printHexA
+;;     Space : ldy #2 : lda offsets,y : jsr printHexA
+;;     Space : ldy #3 : lda offsets,y : jsr printHexA
+;;     Space : ldy #4 : lda offsets,y : jsr printHexA
+;;     rts
 
 .selectBuffer: ; X:buf# --> bufP (use A,Y)
     txa : asl a : tay
@@ -239,6 +299,7 @@ PRINT numberOfBuffers
 ;;; TODO: do we need to push bufO back into offsets???
 ;;; Once we are done filling a buffer, we process from the start anyway!
 ;;; only reason is to see how many writes we made
+;;; TODO: when we have sep buffers per mask, then we will have multiple 'open' at once
 .closeB: ; X:buf#, bufO (use A)
     lda bufO : sta offsets,x
     rts
@@ -257,19 +318,20 @@ macro WriteB ; bufO/bufP, A:data (use y)
 endmacro
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Calculate screen address from X/Y
+;;; Calculate screen address from X/Y (ORIG, and faster!)
 
-.calcA: ; theX/theY --> theA
+.calcA: ; theX/theY --> the{A,FX,CX,FY,CY}
     {
     lda theX+1
     lsr a
-    sta smc_cx+1
+    sta smc_cx+1 : sta theCX
     lsr a : lsr a : lsr a : lsr a
     sta smc_hbOnRow+1
 
     lda theY+1
     lsr a : lsr a : lsr a
-    sta smc_cyA+1
+    sta theCY
+    sta smc_cyA+1 ; TODO: dont bother with smc since I am maintaining zero-page vars
     sta smc_cyB+1
     asl a : asl a : clc
     .smc_cyA : adc #&ee
@@ -285,37 +347,128 @@ endmacro
     sta smc_alo+1
 
     lda theY+1
-    and #&7
+    and #&7 : sta theFY
     clc : .smc_alo : adc #&ee       ; adjust A with fineY
     sta theA
 
     lda theX ; (only need hi-order bit; rotate into carry)
     asl a
     lda theX+1
-    rol a : and #&3
-    tay
-    lda masks,y
-    sta theFineXmask
+    rol a : and #&3 : sta theFX
 
     rts
+    }
 
 .masks:
     EQUB &88, &44, &22, &11
-    }
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; mod160 -- TODO: do better!
+;;; incremental movement
 
-.mod160: {
-    m = 160
-    x = 9
-    clc : adc #x
-    { cmp #(m+x) : bcc no : sbc #m : .no } ; wrap
-    { cmp #x : bcs no : adc #m : .no } ; unwrap
-    sec : sbc #x
-.done:
+;; NEW, called from drawOutline    
+.moveLeft:
+    lda theX   : sec : sbc #&80                                          : sta theX
+    lda theX+1 :       sbc   #0 : { cmp #&ff : bne no : lda #159 : .no } : sta theX+1
     rts
-    }
+    
+.moveRight:
+    lda theX   : clc : adc #&80                                        : sta theX
+    lda theX+1 :       adc   #0 : { cmp #160 : bne no : lda #0 : .no } : sta theX+1
+    rts
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; incremental movement -- copied from game3
+
+.left1: {
+    lda theFX : bne no
+    jsr left4
+    lda #4 : sta theFX
+.no:
+    dec theFX
+    rts }
+
+.left4:
+    lda theCX
+    ; unwrap line
+    { bne no : lda #80 : sta theCX : jsr downA8 : .no }
+    dec theCX
+    lda theA : sec : sbc #8 : sta theA
+    { bcs no : dec theA+1 : .no }
+    rts
+
+.right1: {
+    inc theFX
+    lda theFX : cmp #4 : bne no
+    lda #0 : sta theFX
+    jsr right4
+.no:
+    rts }
+
+.right4:
+    inc theCX
+    lda theA : clc : adc #8 : sta theA
+    { bcc no : inc theA+1 : .no }
+    lda theCX : cmp #80
+    ; wrap line
+    { bne no : lda #0 : sta theCX : jsr upA8 : .no }
+    rts
+    
+.upA8: ; A -- TODO:inline?
+    lda theA : sec : sbc #&80 : sta theA
+    lda theA+1     : sbc #2   : sta theA+1
+    rts
+
+.downA8: ; A -- TODO:inline?
+    lda theA : clc : adc #&80 : sta theA
+    lda theA+1     : adc #2   : sta theA+1
+    rts
+
+.up1: { ; FY,CY,A
+    lda theFY : bne no
+    lda #7 : sta theFY
+    lda theA : clc : adc #7 : sta theA
+    jmp up8
+.no:
+    dec theA
+    dec theFY
+    rts }
+
+.up8: ; CY,A
+    jsr unwrapScreen
+    dec theCY
+    jmp upA8
+
+.unwrapScreen: { ; CY,A
+    lda theCY
+    bne no
+    lda #32 : sta theCY
+    lda theA+1 : clc : adc #HI(screenEnd-screenStart) : sta theA+1
+.no:
+    rts }
+
+.down1: { ; FY,CY,A
+    inc theA
+    inc theFY
+    lda theFY : cmp #8 : beq cont
+    rts
+.cont:
+    lda #0 : sta theFY
+    lda theA : sec : sbc #8 : sta theA
+    jmp down8 }
+
+.down8: ; CY,A
+    inc theCY
+    jsr downA8
+    jmp wrapScreen
+
+.wrapScreen: { ; CY,A
+    lda theCY
+    cmp #32 : bne no
+    lda #0 : sta theCY
+    lda theA+1 : sec : sbc #HI(screenEnd-screenStart) : sta theA+1
+.no:
+    rts }
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; outlines
@@ -332,8 +485,13 @@ SE = S or E
 NW = N or W
 SW = S or W
 
-.smallRockOutline: ; 22 (&16) points
+.smallRockOutline:
     equb START,E,SE,NE,E,SE,S, SE,S,SW,S,SW, W,NW,SW,W, NW,NE,NW,NW,N,NE, 0
+
+.mediumRockOutline:
+    equb START, E,E,NE,NE,E,E,E,E,SE,SE,E,SE,SE, SW,SW,SW,SE,SE,SE,S,S,SW,SW,SW,W,W
+    equb NW,NW,SW,SW,W,W,W, NW,NW,NW,NE,NE,NW,NW,NW,N,N,NE,NE, 0
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; draw
@@ -344,64 +502,46 @@ SW = S or W
 .asEast equb E
 .asWest equb W
 
-.theN skip 1 ; TODO zero page
-
 .rts:
     rts
 
-.drawShape: { ; into open buffer; (starting)theX/theY -- get modified (use theN)
+.drawOutline: { ; into open buffer; (starting)theX/theY -- get modified (use outlineIndex)
     ldx #0
 .loop:
-    ;Emit 'D'
+    ;cpx #1 : beq rts ;; DEV HACK - just see one point
     .*SMC_outline : lda &7777, x : beq rts
     inx
-    stx theN
-
+    stx outlineIndex
+    sta outlineMotion
     bit asNorth : bne north
     bit asSouth : bne south
     jmp ew
 .north:
-    ;Emit 'N'
-    dec theY+1
+    jsr up1
     jmp ew
 .south:
-    ;Emit 'S'
-    inc theY+1
-    ;;jmp ew
+    jsr down1
 .ew:
-    pha
+    lda outlineMotion
     bit asEast : bne east
     bit asWest : bne west
     jmp pr
 .east:
-    ;Emit 'E'
-    lda theX
-    clc : adc #128
-    sta theX
-    bcc pr
-    lda theX+1
-    clc : adc #1 : jsr mod160
-    sta theX+1
+    jsr right1 ; TODO: inline?
     jmp pr
 .west:
-    ;Emit 'W'
-    lda theX
-    sec : sbc #128
-    sta theX
-    bcs pr
-    lda theX+1
-    sec : sbc #1 : jsr mod160
-    sta theX+1
-    ;;jmp pr
+    jsr left1 ; TODO: inline?
 .pr:
-    pla
+    lda outlineMotion
     bit invisible : bne next
-    jsr calcA ;; TODO be incremental
     lda theA+1 : WriteB ; A-hi first
     lda theA : WriteB
-    lda theFineXmask : WriteB
+
+    ldy theFX
+    lda masks,y
+    WriteB  ;; TODO: pick buffer based on mask!!!
 .next:
-    ldx theN
+    ldx outlineIndex
     jmp loop
     }
 
@@ -410,8 +550,8 @@ SW = S or W
 
 .plotRock: ;X:buf#
     {
-    Copy16i smallRockOutline, SMC_outline+1
-    jsr drawShape
+    Copy16i mediumRockOutline, SMC_outline+1
+    jsr drawOutline
     rts
     }
 
@@ -434,75 +574,75 @@ SW = S or W
     rts
     }
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; control
 
+.nope: lda #0 : rts
+.edgeLeft: lda keyLeftLAST : bne nope : lda keyLeft : rts
+.edgeRight: lda keyRightLAST : bne nope : lda keyRight : rts
+.edgeUp: lda keyUpLAST : bne nope : lda keyUp : rts
+.edgeDown: lda keyDownLAST : bne nope : lda keyDown : rts
+    
+    
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; main/loop
 
-;;.frameCounter skip 1
+;;; track positions of rocks
 
-.info:
-    ;;Position 1,30 : lda frameCounter : jsr printHexA
-    rts
-
-;;; track positions of rocks with a single number
-.rock1 skip 1
-.rock2 skip 1
-.rock3 skip 1
-.rock4 skip 1
-
-.initPositions:
-    lda #10 : sta rock1
-    lda #10 : sta rock2
-    rts
+.rock1X skip 2
+.rock1Y skip 2
     
-.updatePositions:
-    inc rock1
-    inc rock2
-    inc rock3 : inc rock3
-    dec rock4
+.initPositions:
+    Copy16i 10, rock1X
+    Copy16i 10, rock1Y
     rts
 
+.rock1Up: dec rock1Y+1 : rts
+.rock1Down: inc rock1Y+1 : rts
+
+.rock1Left:
+    Copy16v rock1X, theX
+    jsr moveLeft
+    Copy16v theX, rock1X
+    rts
+
+.rock1Right:
+    Copy16v rock1X, theX
+    jsr moveRight
+    Copy16v theX, rock1X
+    rts
+
+.updatePositions:
+    jsr edgeUp : { beq no : jsr rock1Up : .no }
+    jsr edgeDown : { beq no : jsr rock1Down : .no }
+    jsr edgeLeft : { beq no : jsr rock1Left : .no }
+    jsr edgeRight : { beq no : jsr rock1Right : .no }
+    rts
+
+.plotRock1:
+    Copy16v rock1X, theX
+    Copy16v rock1Y, theY
+    jsr calcA
+    jmp plotRock
+    
 B1 = 1
 B2 = 2
 B3 = 3
 
-.plotRock1: ; move vertical
-    lda #0 : sta theX
-    lda #50 : sta theX+1
-    lda rock1 : sta theY+1
-    jmp plotRock
-
-.plotRock2: ; move horizontal
-    lda #0 : sta theX
-    lda rock2 : sta theX+1
-    lda #50 : sta theY+1
-    jmp plotRock
-
-.plotRock3: ; move vertical
-    lda #0 : sta theX
-    lda #100 : sta theX+1
-    lda rock3 : sta theY+1
-    jmp plotRock
-	
-.plotRock4: ; move horizontal
-    lda #0 : sta theX
-    lda rock4 : sta theX+1
-    lda #100 : sta theY+1
-    jmp plotRock
-
-
 .plotFirstHalf:
+    Ula blue
     jsr plotRock1
-    jsr plotRock2
     lda #0 : WriteB
+    Ula black
     rts
-	
-.plotSecondHalf:
-    jsr plotRock3
-    jsr plotRock4
-    lda #0 : WriteB
-    rts
-    
+
+;; .plotSecondHalf:
+;;     Ula blue
+;;     jsr plotRock2
+;;     lda #0 : WriteB
+;;     Ula black
+;;     rts
+
 .render12:
     Ula magenta
     ldx #B1
@@ -512,65 +652,93 @@ B3 = 3
     Ula black
     rts
 
-.render23:
-    Ula magenta
-    ldx #B2
-    jsr renderWithEor
-    ldx #B3
-    jsr renderWithEor
+;; .render23:
+;;     Ula magenta
+;;     ldx #B2
+;;     jsr renderWithEor
+;;     ldx #B3
+;;     jsr renderWithEor
+;;     Ula black
+;;     rts
+
+;; .render31:
+;;     Ula magenta
+;;     ldx #B3
+;;     jsr renderWithEor
+;;     ldx #B1
+;;     jsr renderWithEor
+;;     Ula black
+;;     rts
+
+.info:
+    Ula green
+    Position 1,1
+    jsr printKeyState
+    ;;Space : lda theX+1 : jsr printHexA : lda theX : jsr printHexA
+    ;;Space : lda theY+1 : jsr printHexA : lda theY : jsr printHexA
+    Space : lda theCX : jsr printHexA : Space : lda theFX : jsr printHexA
+    Space : lda theCY : jsr printHexA : Space : lda theFY : jsr printHexA
+    Space : lda theA+1 : jsr printHexA : lda theA : jsr printHexA
     Ula black
     rts
 
-.render31:
-    Ula magenta
-    ldx #B3
-    jsr renderWithEor
-    ldx #B1
-    jsr renderWithEor
-    Ula black
-    rts
-    
 .main: {
     jsr init
     jsr initPositions
 
-
 .loop:
+    jsr readKeys
     jsr updatePositions
     ldx #B1 : jsr openBstart
     jsr plotFirstHalf
     jsr syncVB
+    ;jsr info
+    jsr render12
+
+    jsr readKeys
+    jsr updatePositions
+    ldx #B2 : jsr openBstart
+    jsr plotFirstHalf
+    jsr syncVB
+    ;jsr info
     jsr render12
     
-    jsr updatePositions
-    ldx #B2 : jsr openBstart
-    jsr plotSecondHalf
-    jsr syncVB
-    jsr render23
 
-    jsr updatePositions
-    ldx #B3 : jsr openBstart
-    jsr plotFirstHalf
-    jsr syncVB
-    jsr render31
-	
-    jsr updatePositions
-    ldx #B1 : jsr openBstart
-    jsr plotSecondHalf
-    jsr syncVB
-    jsr render12
-	
-    jsr updatePositions
-    ldx #B2 : jsr openBstart
-    jsr plotFirstHalf
-    jsr syncVB
-    jsr render23
+    ;; jsr updatePositions
+    ;; ldx #B1 : jsr openBstart
+    ;; jsr plotFirstHalf
+    ;; jsr syncVB
+    ;; jsr render12
 
-    jsr updatePositions
-    ldx #B3 : jsr openBstart
-    jsr plotSecondHalf
-    jsr syncVB
-    jsr render31
+    ;; jsr updatePositions
+    ;; ldx #B2 : jsr openBstart
+    ;; jsr plotSecondHalf
+    ;; jsr syncVB
+    ;; jsr render23
+
+    ;; jsr updatePositions
+    ;; ldx #B3 : jsr openBstart
+    ;; jsr plotFirstHalf
+    ;; jsr syncVB
+    ;; jsr render31
+
+    ;; jsr updatePositions
+    ;; ldx #B1 : jsr openBstart
+    ;; jsr plotSecondHalf
+    ;; jsr syncVB
+    ;; jsr render12
+
+    ;; jsr updatePositions
+    ;; ldx #B2 : jsr openBstart
+    ;; jsr plotFirstHalf
+    ;; jsr syncVB
+    ;; jsr render23
+
+    ;; jsr updatePositions
+    ;; ldx #B3 : jsr openBstart
+    ;; jsr plotSecondHalf
+    ;; jsr syncVB
+    ;; jsr render31
 
     jmp loop
     }
