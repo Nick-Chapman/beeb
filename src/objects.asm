1160e2b618a4035f7743334b0e9748aea8d3e984
Nick Chapman <nic@amadido.co.uk>
2024-01-19 12:26:31 +0000

    keyboard control; object state; dev/debug

--- src/objects.asm
+++ src/objects.asm
@@ -151,39 +151,189 @@ skip 1000
     jsr cursorOff
     jsr replaceWhiteWithCyan
     Copy16i myIRQ, irq1v
+    ;; start in keys mode
+    lda #%00000011 : sta system_VIA_portB ; set bit 3 to 0
+    lda #%01111111 : sta system_VIA_dataDirectionA ; (top bit input)
     rts
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Objects...
+;;; Sounds
+
+.playSounds:
+    ;; TODO!
+    rts
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Keys
+
+.startKeys:
+
+.keyCaps  EQUB 0
+.keyCtrl  EQUB 0
+.keyShift EQUB 0
+.keyEnter EQUB 0
+
+.keyUp    equb 0
+.keyDown  equb 0
+.keyLeft  equb 0
+.keyRight equb 0
+.keyTab   equb 0
+
+.endKeys:
+numberKeys = endKeys - startKeys
+skip numberKeys ;; space to store LAST state
+
+macro PollKey Code, Key
+    lda #0 : sta Key
+    lda #(-Code-1) : sta system_VIA_portA : lda system_VIA_portA
+    bpl no : lda #1 : sta Key : .no
+endmacro
+
+macro Edge Key
+    lda Key : sta Key+numberKeys
+endmacro
+
+.readKeys:
+	;; Caps/Ctrl/Shift are level sensitive keys
+    Edge keyCaps ;; For Debug/Dev - switch arrow lock
+    Edge keyShift ;; For Debug/Dev - switch active state
+    Edge keyEnter
+    Edge keyTab
+    ;; We may or may not want (debugging) arrow keys to be edge sensitive
+    Edge keyUp
+    Edge keyDown
+    Edge keyLeft
+    Edge keyRight
+
+    PollKey -65,  keyCaps
+    PollKey -2,   keyCtrl
+    PollKey -1,   keyShift
+    PollKey -74,  keyEnter
+    PollKey -97,  keyTab
+    PollKey -58,  keyUp
+    PollKey -42,  keyDown
+    PollKey -26,  keyLeft
+    PollKey -122, keyRight
+    rts
+
+macro CheckPress Key ; -> NZ
+    lda Key+numberKeys : eor #&ff : and Key
+endmacro
+
+.printKeyState:
+    lda #'.' : { ldy keyCaps : beq no : lda #'A' : .no } : jsr emit
+    lda #'.' : { ldy keyCtrl : beq no : lda #'C' : .no } : jsr emit
+    lda #'.' : { ldy keyShift: beq no : lda #'S' : .no } : jsr emit
+    lda #'.' : { ldy keyEnter: beq no : lda #'E' : .no } : jsr emit
+    Space
+    lda #'.' : { ldy keyTab  : beq no : lda #'T' : .no } : jsr emit
+    Space
+    lda #'.' : { ldy keyLeft : beq no : lda #'L' : .no } : jsr emit
+    lda #'.' : { ldy keyRight: beq no : lda #'R' : .no } : jsr emit
+    lda #'.' : { ldy keyUp   : beq no : lda #'U' : .no } : jsr emit
+    lda #'.' : { ldy keyDown : beq no : lda #'D' : .no } : jsr emit
+    rts
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Global state
+
+.selectedN: equb 1
+
+.stepSelectedObject:
+    inc selectedN
+    lda selectedN : cmp #NumberObjects : { bne no : lda #0 : sta selectedN : .no }
+    rts
+
+.updateSelectedObjectOnTab:
+    CheckPress keyTab
+    { beq no : jsr stepSelectedObject : .no }
+    rts
+
+.updateGlobalState:
+    jsr updateSelectedObjectOnTab
+    rts
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Objects... state tables & update
 
 NumberObjects = 3
 
-;;; more tables: postions, aciveNow, activeNext, lockedToArrows(DEV)
+;;; tables...
+.isArrowLocked skip NumberObjects
+.isActiveNext skip NumberObjects
+.isActiveNow skip NumberObjects
+
+.placeHolderPos skip NumberObjects ;; TODO placeholder. needs x/y and double precision
+
+macro ToggleA
+	eor #1 : and #1
+endmacro
+
+.updateActivenessOnShift: {
+    CheckPress keyShift : beq no
+    cpx selectedN : bne no
+    lda isActiveNext, x : ToggleA : sta isActiveNext, x
+.no:
+    rts
+    }
+
+.updateArrowLockOnCaps: {
+    CheckPress keyCaps : beq no
+    cpx selectedN : bne no
+    lda isArrowLocked, x : ToggleA : sta isArrowLocked, x
+.no:
+    rts
+    }
+
+.updatePositionIfArrowLocked: {
+    lda isArrowLocked, x : beq no
+    ;; any arrow key for DEV
+    CheckPress keyLeft : bne yes
+    CheckPress keyRight : bne yes
+    CheckPress keyUp : bne yes
+    CheckPress keyDown : bne yes
+.no:
+    rts
+.yes:
+    inc placeHolderPos, x
+    rts
+    }
 
 .updateOb: ; X:ob#
-    Space : Emit 'u' : txa : jsr printHexA
-    rts
-
-.renderOb: ; X:ob#
-    Space : Emit 'r' : txa : jsr printHexA
+    Position 1,30 : Emit 'U' : txa : jsr printHexA
+    jsr updateActivenessOnShift
+    jsr updateArrowLockOnCaps
+    jsr updatePositionIfArrowLocked
     rts
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Update everything.
+;;; Render object
 
-.updateF skip 2*NumberObjects
-.updateN: skip 1
-.updateEverything: {
-    lda #0 : sta updateN
-.loop:
-    ldx updateN : cpx #NumberObjects : { bne no : rts : .no }
-    txa : asl a : tay
-    lda updateF,   y : sta dispatch+1
-    lda updateF+1, y : sta dispatch+2
-    .dispatch : jsr &7777
-    inc updateN
-    jmp loop
-    }
+macro DebugPositionForObject
+    lda #31 : jsr osasci
+    lda #25 : jsr osasci
+    txa : jsr osasci
+endmacro
+
+.debugObject: ; X:ob#
+    DebugPositionForObject
+    txa : jsr printHexA
+    Space : lda #'.' : { cpx selectedN : bne no : lda #'*' : .no } : jsr emit
+    Space : lda #'0' : { ldy isActiveNext, x : beq no : lda #'1' : .no } : jsr emit
+    lda #'0' : { ldy isActiveNow, x : beq no : lda #'1' : .no } : jsr emit
+    Space : lda #'0' : { ldy isArrowLocked, x : beq no : lda #'1' : .no } : jsr emit
+    Space : lda placeHolderPos, x : jsr printHexA
+    rts
+
+.renderOb:
+    Position 1,30 : Emit 'r' : txa : jsr printHexA
+    jsr debugObject
+    ;; TODO:
+    ;; - IF WAS active, plot/(UN)render object outline at LAST position
+    ;; - IF WILLBE active, plot/render object outline at position
+    ;; - copy the position rendered at to LAST position
+    lda isActiveNext, x : sta isActiveNow, x
+    rts
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; IRQ, VBlank syncing
@@ -198,7 +348,7 @@ NumberObjects = 3
     rti
 .vblank:
     sta system_VIA_interruptFlags ; ack
-    lda vsyncNotify : bne crash
+    ;;lda vsyncNotify : bne crash ;; TODO
     inc vsyncNotify
     lda interruptSaveA
     rti
@@ -207,13 +357,42 @@ NumberObjects = 3
     }
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; onSync
+
+.frameCounter: skip 1
+
+.updateF skip 2*NumberObjects
+.updateN: skip 1
+
+.updateObjects: {
+    lda #0 : sta updateN
+.loop:
+    ldx updateN : cpx #NumberObjects : { bne no : rts : .no }
+    txa : asl a : tay
+    lda updateF,   y : sta dispatch+1
+    lda updateF+1, y : sta dispatch+2
+    .dispatch : jsr &7777
+    inc updateN
+    jmp loop
+    }
+
+.onSync:
+    inc frameCounter
+    jsr playSounds
+    jsr readKeys
+    Position 1,1 : lda frameCounter : jsr printHexA : Space : jsr printKeyState
+    jsr updateGlobalState
+    jsr updateObjects
+    rts
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Render loop.
 
 .renderF skip 2*NumberObjects
 .renderN: skip 1
 .renderLoop: {
 .loop:
-    lda vsyncNotify : { beq no : dec vsyncNotify : jsr updateEverything : .no }
+    lda vsyncNotify : { beq no : dec vsyncNotify : jsr onSync : .no }
     lda renderN : cmp #NumberObjects : { bne no : lda #0 : sta renderN : .no }
     tax : asl a : tay
     lda renderF,   y : sta dispatch+1
